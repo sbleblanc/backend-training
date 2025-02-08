8cbbcf69aa47b68cf3598c7c205e0d97ca13f06c
Samuel Beland-Leblanc <samuel@sbl-dev.ca>
2025-02-08 15:48:00 -0500

    Variable rules

--- backend_training/flask_tutorial/hello.py
+++ backend_training/flask_tutorial/hello.py
@@ -8,6 +8,25 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
 @app.route("/")
 def index():
     return "Index Page"
+
+
+@app.route('/user/<username>')
+def show_user_profile(username: str):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id: int):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath: str):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
