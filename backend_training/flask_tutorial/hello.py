b76c6b171d528314b99aa148787b9f54f7a76577
Samuel Beland-Leblanc <samuel@sbl-dev.ca>
2025-02-08 15:41:07 -0500

    Escaping with variable in route

--- backend_training/flask_tutorial/hello.py
+++ backend_training/flask_tutorial/hello.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!!!"
+@app.route("/<name>")
+def hello_world(name):
+    return f"Welcome, {escape(name)}"
