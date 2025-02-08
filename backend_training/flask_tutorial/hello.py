de18b5cc4568f6b54b2854dadca90cd346df3f16
Samuel Beland-Leblanc <samuel@sbl-dev.ca>
2025-02-08 16:28:02 -0500

    URL building

--- backend_training/flask_tutorial/hello.py
+++ backend_training/flask_tutorial/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -30,3 +30,10 @@ def show_post(post_id: int):
 def show_subpath(subpath: str):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('hello'))
+    print(url_for('hello', next='/'))
+    print(url_for('show_user_profile', username='John Doe'))
